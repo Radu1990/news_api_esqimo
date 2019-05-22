a8651100667d2cf2ac4ee7241e1421ea12b94473
Radu1990 <et2323@gmail.com>
2019-05-23 00:21:14 +0200

    update

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# This file is a marker for Python <= 3.2 REGULAR PACKAGES
+# Importing the current package will automatically execute this file and all the other __init__.py files in the
+# package hierarchy.
+
+# The classes, functions and variables defined here will be defined directly in the package root instead of a module
+# inside the package.
