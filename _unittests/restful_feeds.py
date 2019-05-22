221d85b08c314e411e4a327db8a8ac4de80fc000
Radu1990 <et2323@gmail.com>
2019-05-22 20:00:09 +0200

    update

--- _unittests/restful_feeds.py
+++ _unittests/restful_feeds.py
@@ -1,34 +1,35 @@
 import requests
-import _unittests.feed_generator as fg
+
+
+def generate_feed(x):
+    title = ['BBC', 'BBC Technology', 'Reuters', 'Reuters Technology']
+    description = ['BBC News UK', 'BBC News Technology', 'Reuters - UK', 'Reuters Technology']
+    url = ['http://feeds.bbci.co.uk/news/uk/rss.xml',
+           'http://feeds.bbci.co.uk/news/technology/rss.xml',
+           'http://feeds.reuters.com/reuters/UKdomesticNews?format=xml',
+           'http://feeds.reuters.com/reuters/technologyNews?format=xml']
+    category = ['News', 'Technology', 'News', 'Technology']
+
+    return (
+        {
+            "title": title[x],
+            "description": description[x],
+            "url": url[x],
+            "category": category[x]
+        }
+    )
 
 
 def _url(path):
     return 'http://localhost:5000' + path
 
 
-def get_feeds(title="", description="", url="", category=""):
-    payload = {}
-    if title:
-        payload['title'] = title
-    if description:
-        payload['description'] = description
-    if url:
-        payload['url'] = url
-    if category:
-        payload['category'] = category
-
-    if payload:
-        return requests.get(_url('/feed/'), params=payload)
-    else:
-        return requests.get(_url('/feed/'))
-
-
 def describe_feed(feed_id):
     return requests.get('http://localhost:5000/feed/{:d}/'.format(feed_id))
 
 
-def add_random_feed():
-    return add_feed(fg.generate_feed())
+def add_specific_feed(x):
+    return add_feed(generate_feed(x))
 
 
 def add_feed(feed):
@@ -36,24 +37,14 @@ def add_feed(feed):
 
 
 def remove_feed(feed_id):
-    return requests.delete(_url('/booking/{:d}/'.format(feed_id)))
+    return requests.delete(_url('/feed/{:d}/'.format(feed_id)))
 
 
-def update_feed(feed_id, title='Realitatea', description='Stiri online',
-                url='http://rss.realitatea.net/stiri.xml', category='News'):
-    return requests.put(_url('/booking/{:d}/'.format(feed_id)), json={
+def update_feed(feed_id, title, description,
+                url, category):
+    return requests.put(_url('/feed/{:d}/'.format(feed_id)), json={
         "title": title,
         "description": description,
         "url": url,
         "category": category
     })
-
-
-def main():
-
-    print(describe_feed(1))
-
-
-if __name__ == '__main__':
-    main()
-
--- _unittests/test_restful_feeds.py
+++ _unittests/test_restful_feeds.py
@@ -1,29 +1,75 @@
 import _unittests.restful_feeds as rf
 from assertpy import assert_that
 
+# --------------------------------------------------------
 
-def test_feeds_for_other():
-    resp = rf.get_feeds('Mark')
+#
+# def test_add_feed_1():
+#     resp = rf.add_specific_feed(0)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_add_feed_2():
+#     resp = rf.add_specific_feed(1)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_add_feed_3():
+#     resp = rf.add_specific_feed(2)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_add_feed_4():
+#     resp = rf.add_specific_feed(3)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+
+
+# --------------------------------------------------------
+
+
+# def test_removefeed_1():
+#     resp = rf.remove_feed(1)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_removefeed_2():
+#     resp = rf.remove_feed(2)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_removefeed_3():
+#     resp = rf.remove_feed(3)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+#
+#
+# def test_removefeed_4():
+#     resp = rf.remove_feed(4)
+#     assert_that(resp.ok, 'HTTP Request OK').is_true()
+
+
+# --------------------------------------------------------
+
+def test_updatefeed_1():
+    resp = rf.update_feed(1, title='Realitatea', description='Stiri online',
+                          url='http://rss.realitatea.net/stiri.xml', category='News')
     assert_that(resp.ok, 'HTTP Request OK').is_true()
-    for feed in resp.json():
-        assert_that(resp.ok, 'HTTP Request OK').is_true()
-        resp2 = rf.describe_feed(feed['feed_id'])
-        assert_that(resp2.json()["title"], 'title').contains('other')
 
 
-def test_addfeed():
-    resp = rf.add_random_feed()
-    print(resp.json())
+def test_updatefeed_2():
+    resp = rf.update_feed(2, title='Realitatea', description='Stiri online',
+                          url='http://rss.realitatea.net/stiri.xml', category='News')
     assert_that(resp.ok, 'HTTP Request OK').is_true()
 
 
-def test_updatefeed():
-    new_feed = rf.add_random_feed().json()['feed_id']
-    resp = rf.update_feed(new_feed)
+def test_updatefeed_3():
+    resp = rf.update_feed(3, title='Realitatea', description='Stiri online',
+                          url='http://rss.realitatea.net/stiri.xml', category='News')
     assert_that(resp.ok, 'HTTP Request OK').is_true()
 
 
-def test_removefeed():
-    new_feed = rf.add_random_feed().json()['feed_id']
-    resp = rf.remove_feed(new_feed)
+def test_updatefeed_4():
+    resp = rf.update_feed(4, title='Realitatea', description='Stiri online',
+                          url='http://rss.realitatea.net/stiri.xml', category='News')
     assert_that(resp.ok, 'HTTP Request OK').is_true()
+
+
