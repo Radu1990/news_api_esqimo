d4fd5ce1a0dbcac8a7636a3407c59cd079df2c8a
Radu1990 <et2323@gmail.com>
2019-05-23 12:13:21 +0200

    update

--- src/crud.py
+++ src/crud.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
+
 """
 How to use: 
 1. Run $ python3.6
@@ -13,11 +14,13 @@ Use following code in python interactive shell
 3. Run tests
 """
 
+
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, '../db/crud.sqlite')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+guid_db = []
 
 
 # Feeds
@@ -44,6 +47,7 @@ class FeedSchema(ma.Schema):
 
 feed_schema = FeedSchema()
 feeds_schema = FeedSchema(many=True)
+
 """
 This part defined structure of response of our endpoint.
 We want that all of our endpoints will have JSON response.
@@ -63,15 +67,26 @@ def add_feed():
 
     new_feed = Feed(title, description, url, category)
 
-    db.session.add(new_feed)
-    db.session.commit()
+    # we check if url already in db
+    exists = db.session.query(
+        db.session.query(url).exists()
+    ).scalar()
 
-    return jsonify(
-        title=new_feed.title,
-        description=new_feed.description,
-        url=new_feed.url,
-        category=new_feed.category
-    )
+    if not exists:
+        db.session.add(new_feed)
+        db.session.commit()
+
+        return jsonify(
+            title=new_feed.title,
+            description=new_feed.description,
+            url=new_feed.url,
+            category=new_feed.category
+        )
+    else:
+
+        return jsonify(
+            Error='Feed already exists in DB!'
+        )
 
 
 # endpoint to show all feeds
@@ -121,19 +136,65 @@ def feed_delete(id):
 # --------------------------------------------------------
 class FeedEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(120), unique=True)
-    url = db.Column(db.String(120), unique=True)
-    category = db.Column(db.String(80), unique=False)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    url = db.Column(db.String(120), unique=True, nullable=False)
+    pub_date = db.Column(db.String(80), nullable=False)
+    feeds_id = db.Column(db.Integer, db.ForeignKey("Feed.id"), nullable=False)
 
-    def __init__(self, title, description, url, category):
+    def __init__(self, title, description, url, pub_date, guid):
         self.title = title
         self.description = description
         self.url = url
-        self.category = category
+        self.pub_date = pub_date
+        self.guid = guid  # global unique identifier for feed entry
+
+
+class FeedEntrySchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ('title', 'description', 'url', 'pub_date')
+
+
+feed_entry_schema = FeedEntrySchema()
+feeds_entries_schema = FeedEntrySchema(many=True)
+
+
+# endpoint to create new feed entry
+@app.route("/feed/entry/", methods=["POST"])
+def add_feed_entry():
+    title = request.json['title']
+    description = request.json['description']
+    url = request.json['url']
+    pub_date = request.json['pub_date']
+    guid = request.json['guid']
+
+    new_feed_entry = FeedEntry(title, description, url, pub_date, guid)
+
+    # we check if guid already in db
+    exists = db.session.query(
+        db.session.query(guid).exists()
+    ).scalar()
+
+    if not exists:
+        db.session.add(new_feed_entry)
+        db.session.commit()
+
+        return jsonify(
+            title=new_feed_entry.title,
+            description=new_feed_entry.description,
+            url=new_feed_entry.url,
+            category=new_feed_entry.category
+        )
+    else:
+
+        return jsonify(
+            Error='GUID already exists in DB!'
+        )
 
 
 # TODO if ID exists dont add in tabel
+# trebuie facute API rest care sa returneze feed in functie de combinatia aleasa (mai citeste o data ex)
 # GUID numarul trebuie sa fie unic read din baza de date si apoi write daca nu exista
 # 1 git clone
 # 2 totul intr-un readme: se instaleaza dependintele cu venv cu pip install + testing
